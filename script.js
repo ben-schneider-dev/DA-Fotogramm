164b3b1f37118d9c79be1e54b1b479abfc820c3c
Ben Schneider <contact@benschneider-dev.de>
2026-03-03 18:59:22 +0100

    Bilder array erstellt

---
+++ script.js
@@ -0,0 +1,14 @@
+const bilder = [
+    "pic_1.jpg" ,
+    "pic_2.jpg" ,
+    "pic_3.jpg" ,
+    "pic_4.jpg" , 
+    "pic_5.jpg" ,
+    "pic_6.jpg" ,
+    "pic_7.jpg" ,
+    "pic_8.jpg" ,
+    "pic_9.jpg" ,
+    "pic_10.jpg" ,
+    "pic_11.jpg" ,
+    "pic_12.jpg" ,
+];
