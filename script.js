9d140dfc035ba379add684dc30924b270fec2276
Ben Schneider <contact@benschneider-dev.de>
2026-03-23 16:01:19 +0100

    modal oeffnen und schliessen fertig

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 const bilder = [
     "pic_1.jpg" ,
     "pic_2.jpg" ,
@@ -13,6 +14,55 @@ const bilder = [
     "pic_12.jpg" ,
 ];
 
+let aktuellerIndex = 0;
+let modalIstOffen = false;
+
+function modalOeffnen(index) {
+    aktuellerIndex = index;
+    modalIstOffen = true;
+
+    const modal = document.getElementById("modal");
+    const modalBild = document.getElementById("modal-bild");
+
+    modalBild.src ="assets/img/" + bilder[aktuellerIndex];
+    modal.classList.remove("versteckt");
+}
+
+function modalSchliessen(){
+    modalIstOffen = false;
+
+    const modal =document.getElementById("modal");
+    modal.classList.add("versteckt");
+}
+
 function galerieRendern() {
     const galerieDiv = document.getElementById("galerie");
+
+    bilder.forEach(function(dateiname, index) {
+        const bildRahmen = document.createElement("div");
+        bildRahmen.classList.add("thumbnail");
+       
+        bildRahmen.addEventListener("click", function() {
+            modalOeffnen(index);
+        });
+
+        const imgElement = document.createElement("img");
+        imgElement.src = "assets/img/" + dateiname;
+        imgElement.alt = "Foto" + (index + 1);
+
+        bildRahmen.appendChild(imgElement);
+        galerieDiv.appendChild(bildRahmen);
+
+
+    });
+    
 }
+galerieRendern();
+
+document.getElementById("btn-schliessen").addEventListener("click", function() {
+    modalSchliessen();
+});
+
+document.getElementById("modal-hintergrund").addEventListener("click", function() {
+    modalSchliessen();
+});
